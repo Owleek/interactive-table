44e9aa2e89f21f4729b71c6aea305d9548acfcbc
owleek <owleek@mail.ru>
2021-03-21 10:02:29 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import InteractiveTable from './Components/InteractiveTable/InteractiveTable'
+import data from './data.json';
+
+function App() {
+  return (
+    <div className="App">
+      <InteractiveTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/InteractiveTable/InteractiveTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css'
+
+const InteractiveTable = () => {
+    
+    return (
+        <div className='interactiveTable'>
+        </div>
+    )
+}
+
+export default InteractiveTable;
